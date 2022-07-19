84dc21bf03dd8f27989117002f1d70c9a56620df
Amirtha <amirtha4501@gmail.com>
2022-07-19 23:34:22 +0530

    Updated for mongodb setup

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const mongodb = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fqv0ljm.mongodb.net/?retryWrites=true&w=majority`;;
+// mongoose.connect(mongodb).then(() => { console.log("Connected") }).catch((err) => { console.log(err); });
+
 app.set('view engine', 'ejs');
 app.listen(3000);
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fqv0ljm.mongodb.net/?retryWrites=true&w=majority`;;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(err, collection);
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.get('/', (req, res) => {
