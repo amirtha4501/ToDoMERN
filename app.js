3062756574b7235a3d847f59201e63eff94012ef
Amirtha <amirtha4501@gmail.com>
2022-07-23 22:34:44 +0530

    Fixed mongo connection and CRUD endpoints

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Item = require('./models/items');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// const mongodb = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fqv0ljm.mongodb.net/?retryWrites=true&w=majority`;;
-// mongoose.connect(mongodb).then(() => { console.log("Connected") }).catch((err) => { console.log(err); });
 
-app.set('view engine', 'ejs');
-app.listen(3000);
-
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fqv0ljm.mongodb.net/?retryWrites=true&w=majority`;;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log(err, collection);
-  // perform actions on the collection object
-  client.close();
+const mongodb = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.fqv0ljm.mongodb.net/item-database?retryWrites=true&w=majority`;
+// const mongodb = `mongodb+srv://amirtha4501:amirtha4501%40gmail.com@cluster0.fqv0ljm.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongodb, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Connected");
+    app.listen(3000);
+}).catch((err) => {
+    console.log("ERROR -> ", err);
 });
 
+app.set('view engine', 'ejs');
+
+app.get('/create-item', (req, res) => {
+    const item = new Item({
+        name: 'computer',
+        price: 75000
+    });
+    item.save().then(result => res.send(result));
+})
 
 app.get('/', (req, res) => {
-    const items = [
-        { name: 'Laptop', price: 1000 },
-        { name: 'Book', price: 30 },
-        { name: 'Alexa', price: 2000 },
-    ]
-    res.render('index', { items });
+    res.redirect('/get-items');
+})
+
+app.get('/get-items', (req, res) => {
+    Item.find()
+        .then(items => {
+            res.render('index', { items });
+        })
+        .catch(err => console.log(err))
+})
+
+app.get('/get-item', (req, res) => {
+    Item.findById("62dbdf550315ccf6fdbc640b")
+        .then(result => {
+            res.send(result)
+        })
+        .catch(err => console.log(err))
 })
 
 app.get('/add-item', (req, res) => {
